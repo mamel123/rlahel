eb32f8995ce83f4a4bac0a63bb10050d0bccdd0f
mamel123 <56208400+mamel123@users.noreply.github.com>
2019-10-06 08:14:42 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -24,4 +25,5 @@ async def on_message(message):
                                    "\nATS: online")
 
 
-client.run("NjExOTQyMDYzNDI0MDEyMzI0.XZkbYQ.pfNxK_gXPFcShX5_HG3BXNkgPZo")
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
