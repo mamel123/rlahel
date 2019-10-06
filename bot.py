24cbb5963905b5aa59b30d7736f83f74dc90f9cb
mamel123 <56208400+mamel123@users.noreply.github.com>
2019-10-06 18:54:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,17 +15,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.is_private and message.author.id != "611942063424012324":
+        await client.send_message(client.get_channel("630341534436556800"), message.author.name + "(" + message.author.id + ") : " + message.content)
     if message.content.startswith("hacks status"):
-        await message.channel.send("FUSE: offline"
+        await message.channel.send("FUSE: refurbishment"
                                    "\nD3SK1NG: offline"
                                    "\nATS: online")
     if message.content.startswith("핵 현황"):
-        await message.channel.send("FUSE: offline"
+        await message.channel.send("FUSE: refurbishment"
                                    "\nD3SK1NG: offline"
                                    "\nATS: online")
-    if message.content.startswith("데스킹 현황"):
-        await message.channel.send("D3SK1NG: offline")
-
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
