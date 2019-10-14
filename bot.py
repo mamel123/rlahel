33bc3c17eb70c0eae237c91340f951283bfdf417
mamel123 <56208400+mamel123@users.noreply.github.com>
2019-10-14 23:46:41 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,9 +27,12 @@ async def on_message(message):
 
     if message.content.startswith("데스킹 현황"):
         await message.channel.send("D3SK1NG: online")
+        
+    if message.content.startswith("뉴스 현황"):
+        await message.channel.send("현재 새로운 뉴스가 있습니다.)
 
     if message.content.startswith("!news"):
-        await message.channel.send("__**FUSE will be fully fix, re-released**__")
+        await message.channel.send("__**The developer is on vacation, so it takes a while for the FUSE re-release.**__")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
