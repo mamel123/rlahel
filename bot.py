977e1e837af8422be10d28f787743e386a45760c
mamel123 <56208400+mamel123@users.noreply.github.com>
2019-10-15 00:12:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,33 @@ async def on_message(message):
                                    "\n```fix"
                                    "\n!핵 현황"
                                    "\n!뉴스 현황"
-                                   "\n!news```")
+                                   "\n!news
+                                   "\n!fivem```")
+
+        if message.content.startswith("!fivem"):
+        await message.channel.send("```fix"
+                                   "\n※ 안녕하세요 CHANCE 서버 입니다.```**"
+                                   "\n───────────────────────────────────────────────────"
+                                   "\n**```md"
+                                   "\n 1 : 서버 오픈 준비는 많이 걸렸지만 서버 운영과 유저분들의 즐거움을 위한 서버입니다!"
+                                   "\n 2 : 서버 내 불편, 건의, 개선사항을 관리자에게 말씀해주시면 빠른 답변과 업데이트로 플레이하는데 차질없게 하겠습니다!"
+                                   "\n 3 : 유저분들의 즐거운 RP를 위하여 유저를 먼저 생각하는 친절한 관리자들과 친절한 유저들! 함께 서버를 만들어갑시다!"
+                                   "\n 4 : 새로오신 뉴비분들도 불편함 없게 뉴비교육을 실시하고 있습니다!```**"
+                                   "\n───────────────────────────────────────────────────"
+                                   "\n**```yaml"
+                                   "\n《[신생] KR RP CHANCE SERVER》"
+                                   "\n☆ Server Name :  KR RP CHANCE SERVER ```**"
+                                   "\n───────────────────────────────────────────────────"
+                                   "\n**```tex"
+                                   "\n$ 배달부, 택시 , 정비공, 어부 등등 다양한 직업들이 있습니다!"
+
+                                   "\n지나가시던 분들도 한번 오셔서 부족한점 건의해주시면 더 쾌적한 서버를 만들도록 노력하겠습니다. 감사합니다 !```"
+                                   "\n───────────────────────────────────────────────────"
+                                   "\n```css"
+                                   "\n『 디스코드 주소 』"
+                                   "\n※ 디스코드 참여 : https://discord.gg/9CWjZpr"
+                                   "\n※ 입국방법은 디스코드 접속 후 시민 인증 신청```"
+                                   "\n@everyone")
 
     if message.content.startswith("!news"):
         await message.channel.send("__**The developer is on vacation, so it takes a while for the FUSE re-release.**__")
